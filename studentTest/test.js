af8b1b0fb578027f9e710ead77a56a37768e8bb0
Rob <rjamie2070@gmail.com>
2015-03-31 12:25:43 -0700

    Updates

--- studentTest/test.js
+++ studentTest/test.js
@@ -1,3 +1,9 @@
+//   TWO GROUPS FOR THE DRILL:
+
+//  Group 1:   Finished and up to date on projects
+//				Sit right up front near me :)
+
+//  Group 2:  Not finished and still working.  If you are part of this group, you should go start working now!
 
 
-		
+
