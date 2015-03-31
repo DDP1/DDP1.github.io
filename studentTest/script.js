ff95370ee7cc9eaa9a5bd04ebb73cd102c10723f
Rob <rjamie2070@gmail.com>
2015-03-31 08:27:27 -0700

    Drill Updates
    …

---
+++ studentTest/script.js
@@ -0,0 +1,27 @@
+//						Drill:
+
+//   Write a quick program that asks a user how their day was.  This program should then have custom made responses depending on what the user answer to that question is.  It should have more than two responses.
+
+//  How would you begin this problem?
+	//  Start with a prompt?  Make it equal to a variable.  Make the html part first. 
+
+
+//  What will your responses look like?
+
+
+var userResponse = prompt("How is your day?");
+
+if (userResponse === "good") {
+
+	document.getElementById('compResponse').innerHTML = "That's awesome!";	
+
+};
+
+
+
+
+
+
+
+
+
