938b19fcc5fbeaedfac1e5f0b8d2aefdda52b93a
pogodrake <student@it515566.ad.ppsnet>
2015-05-12 09:38:37 -0700

    firstupload

---
+++ aburkett/form.js
@@ -0,0 +1,24 @@
+var form = document.forms.example;
+
+form.addEventListener("submit",mixRate, false);
+
+function mixRate(event) {
+
+	event.preventDefault();
+
+	var tape= {
+
+		rating: form.mixtape.value,
+
+	};
+
+	var text="<p>you rated my mixtape as: "+ tape.rating + ".</p>";
+
+	document.getElementById("results").innerHTML = text;
+
+
+
+
+
+
+}
