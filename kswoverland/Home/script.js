b7940c7a931548936ff3e303f12bf600c304d609
Kaleb Swoverland <kswoverland@gmail.com>
2015-05-21 09:05:33 -0700

    Miscellaneous Updates

--- kswoverland/Home/script.js
+++ kswoverland/Home/script.js
@@ -9,10 +9,12 @@ function cookies(event) {
 		rating: form.graham.value,
 	}
 
-	var text = "<p>Gotta go fast, " + chips.rating + ".</p>";
+	var text = "<p>Gotta go fast, " + chips.rating +".</p>";
 
 	document.getElementById("results").innerHTML = text;
 }
+
+
 
 function check() {
     document.getElementById("myCheck").checked = true;
@@ -23,3 +25,4 @@ function uncheck() {
     document.getElementById("myCheck").checked = false;
     document.getElementById("prob").innerHTML="<p>Jason is NOT a true sandwich</p>";
 }
+
