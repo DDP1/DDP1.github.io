89a1141af047cefe26515026023bfefa807208cf
btorrien <b.torrien@gmail.com>
2015-05-21 09:27:08 -0700

    upload

--- tbruce/js/scripts.js
+++ tbruce/js/scripts.js
@@ -1,6 +1,7 @@
 var score = 0;	
+var outcome ="";
 var usrResponse;
-
+//functions usrRock,usrPaper,usrScissors activate when the corosponding img on the website is clicked and sets the variable usrResponse
 function usrRock () { 
 	usrResponse = "ROCK";
 	playGame();
@@ -18,36 +19,37 @@ function usrScissors () {
 	}
 
 function playGame () {
-
+	//logs to the consol to make sure the playGame function is running
 	console.log("the game is starting");
-
+	//Creates a random number from 1 to 3. 1 is rock, 2 is paper, 3 is scissors
 	var compResponse = Math.floor((Math.random() * 3) + 1);
-	//1 is rock, 2 is paper, 3 is scissors
+	//compares the usrResponce and random compResponce based on the rules of Rock paper Scissors
 	if (usrResponse == "ROCK" && compResponse == "1") {
-		alert('You tie!');
+		outcome= 'You tie';
 	} else if(usrResponse == "ROCK" && compResponse =="2"){
-		alert('You lose!');
+		outcome= 'You lose';
 		score--
 	} else if(usrResponse == "ROCK" && compResponse =="3"){
-		alert('You win!');
+		outcome='You win';
 		score++
 	} else if(usrResponse == "PAPER" && compResponse =="1"){
-		alert('You win!');
+		outcome= 'You win';
 		score++
 	} else if(usrResponse == "PAPER" && compResponse =="2"){
-		alert('You tie!');
+		outcome= 'You tie';
 	} else if(usrResponse == "PAPER" && compResponse =="3"){
-		alert('You lose!');
+		outcome= 'You lose';
 		score--
 	} else if(usrResponse == "SCISSORS" && compResponse =="1"){
-	alert('You lose!');
+		outcome= 'You lose';
 		score--
 	} else if(usrResponse == "SCISSORS" && compResponse =="2"){
-		alert('You win!');
+		outcome= 'You win';
 		score++
 	} else if(usrResponse == "SCISSORS" && compResponse =="3"){
-		alert('You tie!');
+		outcome= 'You tie';
 	};
+	//compResponce and usrResponce are used to determine the correct picture to display in the website
 	if (compResponse == "1") {
 		document.getElementById("compHand").src = "http://megaicons.net/static/img/icons_sizes/8/178/512/rock-paper-scissors-rock-icon.png"
 
@@ -66,6 +68,10 @@ function playGame () {
 	} else{
 		document.getElementById("usrHand").src = "http://megaicons.net/static/img/icons_sizes/8/178/256/rock-paper-scissors-scissors-icon.png"
 	};
+	//the total score and out come of the most recent game are displayed in the website
+	document.getElementById("scoreBox").innerHTML = score;
+	document.getElementById("outcomeBox").innerHTML = outcome;
+	//the 
 	console.log(usrResponse);
 	console.log(compResponse);
 	console.log(score);
